aa09fc6527e6e58606ebd338b544007be1dd0a38
Daniel Valencia <dfvalenciar@unal.edu.co>
2023-06-15 20:05:29 -0500

    Avance final

--- src/components/mostrarProductos/AgregarProductos.jsx
+++ src/components/mostrarProductos/AgregarProductos.jsx
@@ -11,12 +11,55 @@ export const AgregarProductos = ({ productos, click, eliminarProducto, setProduc
     return (
         <div ref={mostrarDiv} className="agregarProductos-container">
             <div className="izquierda-container">
-
+                <div className="comprobar-container">
+                    <div className="imprimir-container">
+                        <h1>La tienda de DANIEL</h1>
+                        <h3>Productos comprados</h3>
+                        <table>
+                            {/* Encabezado de la tabla */}
+                            <thead>
+                            {/* Se define una fila en el encabezado de la tabla */}
+                                <tr>
+                                {/* Se define una celda de encabezado para cada uno*/}
+                                    <th>No.</th>
+                                    <th>Nombre del Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                </tr>
+                            </thead>
+                            {/* Se define el cuerpo de la tabla */}
+                            <tbody>
+                                {productos.map((producto, i) => (
+                                    <tr key={producto.id}>
+                                        {/* Se define una celda de datos para cada uno */}
+                                        <td>{i+1}</td>
+                                        <td>{producto.title}</td>
+                                        <td>${producto.price}</td>
+                                        <td>{producto.addNumber}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                            {/* Se define el pie de la tabla */}
+                            <tfoot>
+                                <tr>
+                                    {/* Ancho de columna extendido */}
+                                    <td className="total" colSpan={2}>
+                                        TOTAL
+                                    </td>
+                                    <td className="total" colSpan={2}>
+                                        ${total}
+                                    </td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                        <h4>Muchas gracias por tu compra!</h4>
+                    </div>
+                </div>
             </div>
-            <div className="desplegable-derecha-productos">
+            <div className="desplegable-productos">
                 <div className="desplegable-header">
                     <h1>
-                    Tienes <span className="total-productos">{productos.length}</span> 
+                    Agregaste <span className="total-productos">{productos.length}</span> 
                     {productos.length < 1 || productos.length > 1
                         ? " productos"
                         : " producto"}
@@ -25,7 +68,7 @@ export const AgregarProductos = ({ productos, click, eliminarProducto, setProduc
                     className="boton-salir"
                     onClick={() => {
                         mostrarDiv.current.classList.add("animate");
-                        setTimeout(() => click(false), 230);
+                        setTimeout(() => click(false), 200);
                     }}
                     >
                     ❌
